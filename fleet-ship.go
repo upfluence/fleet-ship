a1e4edc61a0c43a8dd7861467be59f43ff4ae98a
Alexis Montagne <alexis.montagne@gmail.com>
2014-12-24 16:22:47 +0100

    Use go-fmt over fleet-ship.go

--- fleet-ship.go
+++ fleet-ship.go
@@ -14,140 +14,140 @@
 package main
 
 import (
-  "os"
-  "time"
-  "net"
-  "net/http"
-  "net/url"
-  "github.com/coreos/fleet/client"
-  "github.com/coreos/fleet/job"
-  "github.com/gin-gonic/gin"
+	"github.com/coreos/fleet/client"
+	"github.com/coreos/fleet/job"
+	"github.com/gin-gonic/gin"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
 )
 
 func NewFleetAPIClient(path string) (client.API, error) {
-  ep, _ :=  url.Parse(path)
-  dialUnix := ep.Scheme == "unix" || ep.Scheme == "file"
-  dialFunc := net.Dial
+	ep, _ := url.Parse(path)
+	dialUnix := ep.Scheme == "unix" || ep.Scheme == "file"
+	dialFunc := net.Dial
 
-  if dialUnix {
-    ep.Host = "domain-sock"
-    ep.Scheme = "http"
+	if dialUnix {
+		ep.Host = "domain-sock"
+		ep.Scheme = "http"
 
-    sockPath := ep.Path
+		sockPath := ep.Path
 
-    ep.Path = ""
+		ep.Path = ""
 
-    dialFunc = func(string, string) (net.Conn, error) {
-      return net.Dial("unix", sockPath)
-    }
-  }
+		dialFunc = func(string, string) (net.Conn, error) {
+			return net.Dial("unix", sockPath)
+		}
+	}
 
-  tr := &http.Transport{
-    Dial: dialFunc,
-  }
+	tr := &http.Transport{
+		Dial: dialFunc,
+	}
 
-  cl := http.Client{Transport: tr}
+	cl := http.Client{Transport: tr}
 
-  return client.NewHTTPClient(&cl, *ep)
+	return client.NewHTTPClient(&cl, *ep)
 }
 
 func RenderJSONOrError(c *gin.Context, value interface{}, err error) {
-  if err != nil {
-    c.JSON(500, gin.H{"error":err.Error()})
-  } else {
-    c.JSON(200, value)
-  }
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+	} else {
+		c.JSON(200, value)
+	}
 }
 
 func waitUntilTargetStateReached(client client.API, name, state string) {
-  sleep := 500 * time.Millisecond
+	sleep := 500 * time.Millisecond
 
-  for {
-    unit, err := client.Unit(name)
+	for {
+		unit, err := client.Unit(name)
 
-    if err == nil && unit.CurrentState == state {
-      return
-    }
+		if err == nil && unit.CurrentState == state {
+			return
+		}
 
-    time.Sleep(sleep)
-  }
+		time.Sleep(sleep)
+	}
 }
 
 func main() {
-  endpoint := os.Getenv("FLEET_ENDPOINT")
+	endpoint := os.Getenv("FLEET_ENDPOINT")
 
-  cl,  _ := NewFleetAPIClient(endpoint)
+	cl, _ := NewFleetAPIClient(endpoint)
 
-  routerEngine := gin.Default()
+	routerEngine := gin.Default()
 
-  routerGroup := routerEngine.RouterGroup
+	routerGroup := routerEngine.RouterGroup
 
-  if endpoint == "" {
-    endpoint = "unix:///var/run/fleet.sock"
-  }
+	if endpoint == "" {
+		endpoint = "unix:///var/run/fleet.sock"
+	}
 
-  if os.Getenv("BASIC_USERNAME") != "" || os.Getenv("BASIC_PASSWORD") != "" {
-    routerGroup = routerEngine.Group(
-      "/",
-      gin.BasicAuth(gin.Accounts{
-        os.Getenv("BASIC_USERNAME"):os.Getenv("BASIC_PASSWORD"),
-      }),
-    )
-  }
+	if os.Getenv("BASIC_USERNAME") != "" || os.Getenv("BASIC_PASSWORD") != "" {
+		routerGroup = routerEngine.Group(
+			"/",
+			gin.BasicAuth(gin.Accounts{
+				os.Getenv("BASIC_USERNAME"): os.Getenv("BASIC_PASSWORD"),
+			}),
+		)
+	}
 
-  routerGroup.GET("/machines", func(c *gin.Context) {
-    machines, err := cl.Machines()
+	routerGroup.GET("/machines", func(c *gin.Context) {
+		machines, err := cl.Machines()
 
-    RenderJSONOrError(c, machines, err)
-  })
+		RenderJSONOrError(c, machines, err)
+	})
 
-  routerGroup.GET("/units/:name", func(c *gin.Context) {
-    unit, err := cl.Unit(c.Params.ByName("name"))
+	routerGroup.GET("/units/:name", func(c *gin.Context) {
+		unit, err := cl.Unit(c.Params.ByName("name"))
 
-    RenderJSONOrError(c, unit, err)
-  })
+		RenderJSONOrError(c, unit, err)
+	})
 
-  routerGroup.GET("/units", func(c *gin.Context) {
-    units, err := cl.Units()
+	routerGroup.GET("/units", func(c *gin.Context) {
+		units, err := cl.Units()
 
-    RenderJSONOrError(c, units, err)
-  })
+		RenderJSONOrError(c, units, err)
+	})
 
-  routerGroup.PUT("/deploy/:name", func(c *gin.Context) {
-    var err error
+	routerGroup.PUT("/deploy/:name", func(c *gin.Context) {
+		var err error
 
-    name := c.Params.ByName("name")
+		name := c.Params.ByName("name")
 
-    err = cl.SetUnitTargetState(name, string(job.JobStateLoaded))
+		err = cl.SetUnitTargetState(name, string(job.JobStateLoaded))
 
-    if err != nil {
-      c.JSON(500, gin.H{"error":err.Error()})
-    }
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+		}
 
-    waitUntilTargetStateReached(cl, name, string(job.JobStateInactive))
+		waitUntilTargetStateReached(cl, name, string(job.JobStateInactive))
 
-    err = cl.SetUnitTargetState(name, string(job.JobStateLaunched))
+		err = cl.SetUnitTargetState(name, string(job.JobStateLaunched))
 
-    RenderJSONOrError(c, map[string]bool{"success":true}, err)
-  })
+		RenderJSONOrError(c, map[string]bool{"success": true}, err)
+	})
 
-  routerGroup.PUT("/rebalance/:name", func(c *gin.Context) {
-    var err error
+	routerGroup.PUT("/rebalance/:name", func(c *gin.Context) {
+		var err error
 
-    name := c.Params.ByName("name")
+		name := c.Params.ByName("name")
 
-    err = cl.SetUnitTargetState(name, string(job.JobStateInactive))
+		err = cl.SetUnitTargetState(name, string(job.JobStateInactive))
 
-    if err != nil {
-      c.JSON(500, gin.H{"error":err.Error()})
-    }
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+		}
 
-    waitUntilTargetStateReached(cl, name, string(job.JobStateInactive))
+		waitUntilTargetStateReached(cl, name, string(job.JobStateInactive))
 
-    err = cl.SetUnitTargetState(name, string(job.JobStateLaunched))
+		err = cl.SetUnitTargetState(name, string(job.JobStateLaunched))
 
-    RenderJSONOrError(c, map[string]bool{"success":true}, err)
-  })
+		RenderJSONOrError(c, map[string]bool{"success": true}, err)
+	})
 
-  routerEngine.Run(":8080")
+	routerEngine.Run(":8080")
 }
