72cb36daf5e71569384021e1095265507c5affce
Alexis Montagne <alexis.montagne@gmail.com>
2015-02-17 16:04:30 +0100

    Extract an abstraction over fleet ofthe API endpoints

--- fleet-ship.go
+++ fleet-ship.go
@@ -1,87 +1,29 @@
 /*
-   Copyright 2014 Upfluence, Inc.
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+  Copyright 2014 Upfluence, Inc.
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 */
 
 package main
 
 import (
-	"errors"
-	"github.com/coreos/fleet/client"
-	"github.com/coreos/fleet/job"
 	"github.com/gin-gonic/gin"
-	"net"
-	"net/http"
-	"net/url"
 	"os"
 	"strings"
-	"time"
 )
-
-func NewFleetAPIClient(path string) (client.API, error) {
-	ep, _ := url.Parse(path)
-	dialUnix := ep.Scheme == "unix" || ep.Scheme == "file"
-	dialFunc := net.Dial
-
-	if dialUnix {
-		ep.Host = "domain-sock"
-		ep.Scheme = "http"
-
-		sockPath := ep.Path
-
-		ep.Path = ""
-
-		dialFunc = func(string, string) (net.Conn, error) {
-			return net.Dial("unix", sockPath)
-		}
-	}
-
-	tr := &http.Transport{
-		Dial: dialFunc,
-	}
-
-	cl := http.Client{Transport: tr}
-
-	return client.NewHTTPClient(&cl, *ep)
-}
 
 func RenderJSONOrError(c *gin.Context, value interface{}, err error) {
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 	} else {
 		c.JSON(200, value)
-	}
-}
-
-func assertUnitExistence(client client.API, name string) error {
-	unit, err := client.Unit(name)
-
-	if err == nil && unit == nil {
-		err = errors.New("The unit does not exist")
-	}
-
-	return err
-}
-
-func waitUntilTargetStateReached(client client.API, name, state string) {
-	sleep := 500 * time.Millisecond
-
-	for {
-		unit, err := client.Unit(name)
-
-		if err == nil && unit.CurrentState == state {
-			return
-		}
-
-		time.Sleep(sleep)
 	}
 }
 
@@ -116,64 +58,35 @@ func main() {
 	}
 
 	routerGroup.GET("/machines", func(c *gin.Context) {
-		machines, err := cl.Machines()
+		machines, err := (*cl.Client).Machines()
 
 		RenderJSONOrError(c, machines, err)
 	})
 
 	routerGroup.GET("/units/:name", func(c *gin.Context) {
-		unit, err := cl.Unit(normalizeName(c.Params.ByName("name")))
+		unit, err := (*cl.Client).Unit(normalizeName(c.Params.ByName("name")))
 
 		RenderJSONOrError(c, unit, err)
 	})
 
 	routerGroup.GET("/units", func(c *gin.Context) {
-		units, err := cl.Units()
+		units, err := (*cl.Client).Units()
 
 		RenderJSONOrError(c, units, err)
 	})
 
 	routerGroup.PUT("/deploy/:name", func(c *gin.Context) {
-		var err error
-		name := normalizeName(c.Params.ByName("name"))
-
-		if err = assertUnitExistence(cl, name); err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
+		for _, unit := range cl.FindMatchingUnits(normalizeName(c.Params.ByName("name"))) {
+			go cl.RestartUnit(unit)
 		}
-
-		if err = cl.SetUnitTargetState(name, string(job.JobStateLoaded)); err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
-		}
-
-		go func() {
-			waitUntilTargetStateReached(cl, name, string(job.JobStateLoaded))
-
-			cl.SetUnitTargetState(name, string(job.JobStateLaunched))
-		}()
 
 		c.JSON(200, "Deployment asked")
 	})
 
 	routerGroup.PUT("/rebalance/:name", func(c *gin.Context) {
-		var err error
-
-		name := normalizeName(c.Params.ByName("name"))
-
-		if err = assertUnitExistence(cl, name); err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
+		for _, unit := range cl.FindMatchingUnits(normalizeName(c.Params.ByName("name"))) {
+			go cl.RebalanceUnit(unit)
 		}
-
-		err = cl.SetUnitTargetState(name, string(job.JobStateInactive))
-
-		if err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
-		}
-
-		go func() {
-			waitUntilTargetStateReached(cl, name, string(job.JobStateInactive))
-
-			cl.SetUnitTargetState(name, string(job.JobStateLaunched))
-		}()
 
 		c.JSON(200, "Rebalancing asked")
 	})
---
+++ fleet_client.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"fmt"
+	"github.com/coreos/fleet/client"
+	"github.com/coreos/fleet/job"
+	"net"
+	"net/http"
+	"net/url"
+	"regexp"
+	"time"
+)
+
+type FleetClient struct {
+	Client *client.API
+}
+
+func NewFleetAPIClient(path string) (*FleetClient, error) {
+	ep, _ := url.Parse(path)
+	dialUnix := ep.Scheme == "unix" || ep.Scheme == "file"
+	dialFunc := net.Dial
+
+	if dialUnix {
+		ep.Host = "domain-sock"
+		ep.Scheme = "http"
+
+		sockPath := ep.Path
+
+		ep.Path = ""
+
+		dialFunc = func(string, string) (net.Conn, error) {
+			return net.Dial("unix", sockPath)
+		}
+	}
+
+	tr := &http.Transport{
+		Dial: dialFunc,
+	}
+
+	cl := http.Client{Transport: tr}
+
+	httpClient, err := client.NewHTTPClient(&cl, *ep)
+
+	if err != nil {
+		return nil, err
+	} else {
+		return &FleetClient{&httpClient}, nil
+	}
+}
+
+func (c *FleetClient) FindMatchingUnits(name string) []string {
+	if exist, _ := c.AssertUnitExistence(name); exist {
+		return []string{name}
+	} else {
+		units, _ := c.SubUnits(name)
+
+		return units
+	}
+}
+
+func (c *FleetClient) AssertUnitExistence(name string) (bool, error) {
+	unit, err := (*c.Client).Unit(name)
+
+	if unit != nil {
+		return true, err
+	} else {
+		return false, err
+	}
+}
+
+func (c *FleetClient) SubUnits(name string) ([]string, error) {
+	units, err := (*c.Client).Units()
+
+	if err != nil {
+		return []string{}, err
+	}
+
+	subUnitRegexp, err := regexp.Compile(fmt.Sprintf("^%s@.+", name))
+
+	if err != nil {
+		return []string{}, err
+	}
+
+	result := []string{}
+
+	for _, unit := range units {
+		if subUnitRegexp.MatchString(unit.Name) {
+			result = append(result, unit.Name)
+		}
+	}
+
+	return result, nil
+}
+
+func (c *FleetClient) WaitUntilTargetStateReached(name, state string) {
+	sleep := 500 * time.Millisecond
+
+	for {
+		unit, err := (*c.Client).Unit(name)
+
+		if err == nil && unit.CurrentState == state {
+			return
+		}
+
+		time.Sleep(sleep)
+	}
+}
+
+func (c *FleetClient) SwapStateUnit(name string, beforeState, afterState job.JobState) error {
+	if err := (*c.Client).SetUnitTargetState(name, string(beforeState)); err != nil {
+		return err
+	}
+
+	c.WaitUntilTargetStateReached(name, string(beforeState))
+
+	if err := (*c.Client).SetUnitTargetState(name, string(afterState)); err != nil {
+		return err
+	}
+
+	c.WaitUntilTargetStateReached(name, string(afterState))
+
+	return nil
+}
+
+func (c *FleetClient) RestartUnit(name string) error {
+	return c.SwapStateUnit(name, job.JobStateLoaded, job.JobStateLaunched)
+}
+
+func (c *FleetClient) RebalanceUnit(name string) error {
+	return c.SwapStateUnit(name, job.JobStateInactive, job.JobStateLaunched)
+}
